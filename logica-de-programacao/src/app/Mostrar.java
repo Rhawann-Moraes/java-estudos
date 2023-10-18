e321024fffae5225e3614cd42d36db89c316a011
Rhawann Moraes <rhawann@gmail.com>
2023-10-18 07:31:30 -0300

    Estudando com intellij

---
+++ logica-de-programacao/src/app/Mostrar.java
@@ -0,0 +1,13 @@
+package app;
+
+public class Mostrar {
+    public static void main(String[] args) {
+        int idade = 31;
+        double altura = 1.81;
+        String nome = "Rhawann";
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+    }
+}
