e0d8d70852a365dd98c9f3e4349c3a6fa2e955b4
Rhawann Moraes <rhawann@gmail.com>
2023-10-18 07:40:15 -0300

    Declarando variáveis

---
+++ logica-de-programacao/src/TiposDeVariaveis.java
@@ -0,0 +1,9 @@
+public class TiposDeVariaveis {
+    public static void main(String[] args) {
+        int idade = 31;
+        double altura = 1.86;
+        String nome = "Rhawann";
+
+        System.out.println("O " + nome + " tem " + idade + " anos de idade e tem " + altura + "m de altura.");
+    }
+}
