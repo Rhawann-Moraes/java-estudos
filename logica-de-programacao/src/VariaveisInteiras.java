4449320d14d81d39b9ea453c9dbcfed437226914
Rhawann Moraes <rhawann@gmail.com>
2023-10-20 09:16:03 -0300

    tipos inteiros

---
+++ logica-de-programacao/src/VariaveisInteiras.java
@@ -0,0 +1,13 @@
+public class VariaveisInteiras {
+    public static void main (String[] args){
+        byte idade1 = 20;
+        short idade2 = 21;
+        int idade3 = 22;
+        long idade4 = 23;
+
+        System.out.println("O valor da idade em byte é: " +idade1);
+        System.out.println("O valor da idade em short é: " +idade2);
+        System.out.println("O valor da idade em int é: " +idade3);
+        System.out.println("O valor da idade em long é: " +idade4);
+    }
+}
