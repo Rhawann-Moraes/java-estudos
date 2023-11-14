e51bbea09c562f50fafd6f392be4f3e1c3ad1659
Rhawann Moraes <rhawann@gmail.com>
2023-11-14 15:24:44 -0300

    avançando com o exercício

--- vainaweb/src/megasena/megaSena.java
+++ vainaweb/src/megasena/megaSena.java
@@ -1,6 +1,7 @@
 package megasena;
 
 import java.util.InputMismatchException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class megaSena
@@ -9,25 +10,15 @@ public class megaSena
 	{
 		Scanner scanner = new Scanner(System.in);
 		
-		int[]numerosEscolhidos = obterNumeros("Insira os números: ", scanner);
+		int [] numerosEscolhidos = obterNumeros("Insira os números: ", scanner);
 		
-		System.out.println("Números escolhidos");
-		for (int i = 0; i < numerosEscolhidos.length; i++) 
-		{
-			if(i == 0)
-			{
-				System.out.print( "(" + numerosEscolhidos[i] + " - ");
-			}
-			else if(i == numerosEscolhidos.length -1)
-			{
-				System.out.println(numerosEscolhidos[i] + ")");
-			}
-			else
-			{
-				System.out.print(numerosEscolhidos[i] + " - ");
-			}  
-		}
-		scanner.close();
+		int [] numerosSorteados = sorteio();
+		
+		exibir("Esses são os seus números escolhidos: ", numerosEscolhidos);
+		exibir("Esses são os seus números sorteados: ", numerosSorteados);
+		
+		
+		
 	}
 	
 	public static int[] obterNumeros(String mensagem, Scanner scanner)
@@ -72,5 +63,43 @@ public class megaSena
 			}
 		}
 		return false;
+	}
+	
+	public static int [] sorteio()
+	{
+		int tamanho = 7;
+		int numerosSorteados[]= new int[tamanho];
+		Random random = new Random();
+		
+		for (int i = 0; i < tamanho; i++) {
+            int numeroSorteado;
+            do {
+                numeroSorteado = random.nextInt(100) + 1;
+            } while (numerosIguais(numerosSorteados, numeroSorteado));
+
+            numerosSorteados[i] = numeroSorteado;
+        }
+
+        return numerosSorteados;
+    }
+
+	public static void exibir(String mensagem, int [] numeros)
+	{
+		System.out.println(mensagem);
+		for (int i = 0; i < numeros.length; i++) 
+		{
+			if(i == 0)
+			{
+				System.out.print( "(" + numeros[i] + " - ");
+			}
+			else if(i == numeros.length -1)
+			{
+				System.out.println(numeros[i] + ")");
+			}
+			else
+			{
+				System.out.print(numeros[i] + " - ");
+			}  
+		}
 	}
 }
